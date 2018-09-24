fe426035c93a8a70a21ff74556498fc014420cf3
Nishant Nikunja <nishant.nikunja@milvik.se>
2018-09-24 18:15:50 +0530

    done

---
+++ playground/mongoose_query.js
@@ -0,0 +1,41 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('../server/models/todo');
+const {User} = require('../server/models/user');
+
+var id = '5ba8b476789cef3f057a901d';
+
+if(!ObjectID.isValid(id)) {
+  console.log('Id not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos ', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos ', todos);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by Id ', todo);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+
+User.findById('5ba87cc4c0bfa715bef917cc').then((user) => {
+  if(!user) {
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/todo.js');
-var {User} = require('./models/user.js');
+const {mongoose} = require('./db/mongoose.js');
+const {Todo} = require('./models/todo.js');
+const {User} = require('./models/user.js');
+const {ObjectID} = require('mongodb');
 
-var app = express();
+const app = express();
 app.use(bodyParser.json()); //apply middleware
 
 app.post('/todos', (req, res) => {
@@ -22,6 +23,36 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  //get everything from todos collection
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  //sending back request params object
+  //res.send(req.params);
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    return res.ststus(400).send();
+  });
+
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
